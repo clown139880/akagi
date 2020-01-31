bb74f761b6bf62d0cc6aa495e321e6f1c03abce1
UniClown <clown139880@gmail.com>
2020-01-31 10:25:51 +0800

    fix migration

--- db/migrate/20200127102812_create_weight_logs.rb
+++ db/migrate/20200127102812_create_weight_logs.rb
@@ -1,7 +1,7 @@
 class CreateWeightLogs < ActiveRecord::Migration[5.2]
   def change
     create_table :weight_logs, id: :integer do |t|
-      t.decimal :weight
+      t.decimal :weight, precision: 10, scale: 2
       t.integer :user_id, foreign_key: true
 
       t.timestamps
